36203851115e6227e00459665d78569242d985ea
congnghia0609 <congnghia0609@gmail.com>
2021-01-10 14:28:42 +0700

    complete code sync

--- client.py
+++ client.py
@@ -3,6 +3,7 @@
 @since 09/01/2021
 """
 import logging
+import time
 
 import grpc
 from ngrpc import calculator_pb2
@@ -11,12 +12,72 @@ from ngrpc import calculator_pb2_grpc
 
 def call_sum():
     print("Call sum api")
-    with grpc.insecure_channel('localhost:5555') as channel:
-        stub = calculator_pb2_grpc.CalculatorServiceStub(channel)
-        resp = stub.Sum(calculator_pb2.SumRequest(num1=7, num2=6))
+    # with grpc.insecure_channel('localhost:5555') as channel:
+    #     stub = calculator_pb2_grpc.CalculatorServiceStub(channel)
+    #     resp = stub.Sum(calculator_pb2.SumRequest(num1=7, num2=6))
+    channel = grpc.insecure_channel('localhost:3333')
+    stub = calculator_pb2_grpc.CalculatorServiceStub(channel)
+    resp = stub.Sum(calculator_pb2.SumRequest(num1=7, num2=6))
     print("sum api response: ", resp.result)
+
+
+def call_pnd():
+    print("call pnd api")
+    channel = grpc.insecure_channel('localhost:5555')
+    stub = calculator_pb2_grpc.CalculatorServiceStub(channel)
+    resp = stub.PrimeNumberDecomposition(calculator_pb2.PNDRequest(number=120))
+    for r in resp:
+        print("prime number: %s" % r.result)
+
+
+def avg_iterator(n):
+    # list_req = [
+    #     calculator_pb2.AverageRequest(num=5),
+    #     calculator_pb2.AverageRequest(num=10),
+    #     calculator_pb2.AverageRequest(num=15),
+    #     calculator_pb2.AverageRequest(num=20),
+    #     calculator_pb2.AverageRequest(num=25)
+    # ]
+    for i in range(0, n):
+        time.sleep(1)
+        yield calculator_pb2.AverageRequest(num=5*(i+1))
+
+
+def call_average():
+    print("call average api")
+    channel = grpc.insecure_channel('localhost:5555')
+    stub = calculator_pb2_grpc.CalculatorServiceStub(channel)
+    req_iterator = avg_iterator(5)
+    resp = stub.Average(req_iterator)
+    print("Average response %s" % resp.result)
+
+
+def find_max_iterator():
+    list_req = [
+        calculator_pb2.FindMaxRequest(num=5),
+        calculator_pb2.FindMaxRequest(num=10),
+        calculator_pb2.FindMaxRequest(num=1),
+        calculator_pb2.FindMaxRequest(num=6),
+        calculator_pb2.FindMaxRequest(num=9)
+    ]
+    for req in list_req:
+        time.sleep(1)
+        yield req
+
+
+def call_find_max():
+    print("call find max")
+    channel = grpc.insecure_channel('localhost:5555')
+    stub = calculator_pb2_grpc.CalculatorServiceStub(channel)
+    req_iterator = find_max_iterator()
+    responses = stub.FindMax(req_iterator)
+    for resp in responses:
+        print("Max response: %s" % resp.max)
 
 
 if __name__ == '__main__':
     logging.basicConfig()
-    call_sum()
+    # call_sum()
+    # call_pnd()
+    # call_average()
+    call_find_max()
--- server.py
+++ server.py
@@ -3,6 +3,7 @@
 @since 09/01/2021
 """
 import logging
+import time
 from concurrent import futures
 
 import grpc
@@ -15,6 +16,40 @@ class CalculatorService(calculator_pb2_grpc.CalculatorServiceServicer):
     def Sum(self, request, context):
         print("Sum called...")
         return calculator_pb2.SumResponse(result=request.num1+request.num2)
+
+    def PrimeNumberDecomposition(self, request, context):
+        print("PrimeNumberDecomposition called...")
+        k = 2
+        n = request.number
+        while n > 1:
+            if n % k == 0:
+                n = n / k
+                time.sleep(1)
+                # send to client
+                print("server response k = %s" % k)
+                yield calculator_pb2.PNDResponse(result=k)
+            else:
+                k = k + 1
+
+    def Average(self, request_iterator, context):
+        print("Average called...")
+        total = 0.0
+        count = 0
+        for req in request_iterator:
+            total = total + req.num
+            count = count + 1
+            print("receive req %s" % req.num)
+        return calculator_pb2.AverageResponse(result=total / count)
+
+    def FindMax(self, request_iterator, context):
+        print("FindMax called...")
+        max = 0
+        for req in request_iterator:
+            num = req.num
+            print("recv num %s" % num)
+            if num > max:
+                max = num
+            yield calculator_pb2.FindMaxResponse(max=max)
 
 
 def serve():
